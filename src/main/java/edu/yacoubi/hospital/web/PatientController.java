0db2ef91c4938caffc609b0f7649ad618431c09c
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-25 23:19:57 +0200

    added paging to patients html view

--- src/main/java/edu/yacoubi/hospital/web/PatientController.java
+++ src/main/java/edu/yacoubi/hospital/web/PatientController.java
@@ -2,12 +2,14 @@ package edu.yacoubi.hospital.web;
 
 import edu.yacoubi.hospital.entities.Patient;
 import edu.yacoubi.hospital.repository.PatientRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -15,10 +17,44 @@ public class PatientController {
 
     private PatientRepository patientRepository;
 
+//    @GetMapping("/index")
+//    public String index(Model model, HttpServletRequest request) {
+//        servlet
+//        int page = Integer.parseInt(request.getParameter("page"));
+//        int size = Integer.parseInt(request.getParameter("size"));
+//        Page<Patient> pagePatients = patientRepository
+//                .findAll(PageRequest.of(page, size));
+//        model.addAttribute("patients", pagePatients.getContent());
+//        return "patients";
+//    }
+
+//    @GetMapping("/index")
+//    public String index(Model model,int page, int size) {
+//        // http://localhost:9090/index?page=0&size=4
+//        Page<Patient> pagePatients = patientRepository
+//                .findAll(PageRequest.of(page, size));
+//        model.addAttribute("patients", pagePatients.getContent());
+//        return "patients";
+//    }
+
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients", patients);
+    public String index(Model model,
+                        // assign parameter page from request to variable p
+                        @RequestParam(name = "page", defaultValue="0") int p,
+                        @RequestParam(name = "size", defaultValue="3") int s) {
+        // http://localhost:9090/index?page=0&size=4
+        // http://localhost:9090/index?size=3
+        Page<Patient> pagePatients = patientRepository
+                .findAll(PageRequest.of(p, s));
+        model.addAttribute(
+                "patients",
+                pagePatients.getContent()
+        );
+        model.addAttribute(
+                "pages",
+                new int[pagePatients.getTotalPages()]
+        );
+        model.addAttribute("currentPage", p);
         return "patients";
     }
 }
